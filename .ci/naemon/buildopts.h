f36f5dd00b1c5cc2478debc7a94296514704b479
Johannes Drummer <j.drummer@thelambda.de>
2018-04-22 19:43:49 +0200

    test ci without compiling naemon

---
+++ .ci/naemon/buildopts.h
@@ -0,0 +1,9 @@
+#ifndef NAEMON_BUILDOPTS_H__
+#define NAEMON_BUILDOPTS_H__
+#define NAEMON_SYSCONFDIR "/opt/naemon/etc/naemon"
+#define NAEMON_LOCALSTATEDIR "/opt/naemon/var"
+#define NAEMON_LOGDIR "/opt/naemon/var/log/naemon"
+#define NAEMON_LOCKFILE "/opt/naemon/var/cache/naemon/naemon.pid"
+#define NAEMON_GROUP "naemon"
+#define NAEMON_USER "naemon"
+#endif
