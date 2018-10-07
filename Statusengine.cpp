78f75fe9771e03e60d6581807aaf0b7b55b0a583
Johannes Drummer <j.drummer@thelambda.de>
2018-10-07 16:14:00 +0200

    improve license message

--- Statusengine.cpp
+++ Statusengine.cpp
@@ -51,6 +51,23 @@ namespace statusengine {
 
         Log() << "the missing event broker" << LogLevel::Info;
         Log() << "This is the c++ version of statusengine event broker" << LogLevel::Info;
+        Log() << "License:\n"
+                 "    statusengine - the missing event broker\n"
+                 "    Copyright (C) 2018  The statusengine team\n"
+                 "\n"
+                 "    This program is free software; you can redistribute it and/or modify\n"
+                 "    it under the terms of the GNU General Public License as published by\n"
+                 "    the Free Software Foundation; either version 2 of the License, or\n"
+                 "    (at your option) any later version.\n"
+                 "\n"
+                 "    This program is distributed in the hope that it will be useful,\n"
+                 "    but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+                 "    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+                 "    GNU General Public License for more details.\n"
+                 "\n"
+                 "    You should have received a copy of the GNU General Public License along\n"
+                 "    with this program; if not, write to the Free Software Foundation, Inc.,\n"
+                 "    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.\n" << LogLevel::Info;
 
         if (!configuration->Load(configurationPath)) {
             return 1;
