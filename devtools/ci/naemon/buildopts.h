d64728a8dd44ae7860c5b786bf1e3a119275382a
Johannes Drummer <j.drummer@thelambda.de>
2019-10-02 12:44:33 +0200

    update ci headers of naemon to 1.1.0

--- devtools/ci/naemon/buildopts.h
+++ devtools/ci/naemon/buildopts.h
@@ -1,9 +1,9 @@
 #ifndef NAEMON_BUILDOPTS_H__
 #define NAEMON_BUILDOPTS_H__
-#define NAEMON_SYSCONFDIR "/opt/naemon/etc/naemon"
-#define NAEMON_LOCALSTATEDIR "/opt/naemon/var"
-#define NAEMON_LOGDIR "/opt/naemon/var/log/naemon"
-#define NAEMON_LOCKFILE "/opt/naemon/var/cache/naemon/naemon.pid"
+#define NAEMON_SYSCONFDIR "/opt/statusengine/etc/naemon"
+#define NAEMON_LOCALSTATEDIR "/opt/statusengine/var"
+#define NAEMON_LOGDIR "/opt/statusengine/var/log/naemon"
+#define NAEMON_LOCKFILE "/opt/statusengine/var/cache/naemon/naemon.pid"
 #define NAEMON_GROUP "jdrummer"
 #define NAEMON_USER "jdrummer"
 #endif
